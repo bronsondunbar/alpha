a449750bc9b97e6fa2b5d1da6051398271f6b99a
bronsondunbar <bronson@proversity.org>
2017-06-28 08:52:18 +0200

    Updates

--- source/js/app.js
+++ source/js/app.js
@@ -109,6 +109,47 @@ $(document).ready(function () {
 
 });
 
+$(document).ready(function () {
+
+  $(".navbar-toggle").click(function (event) {
+    var navbarContent = $(".navbar-content").css("width");
+
+    if (navbarContent === "350px") {
+
+      $(".navbar-content > div").fadeOut(300, function () {
+        $(".navbar-content").animate({
+          width : "0"
+        }, 300);
+      });
+
+    } else if (navbarContent === "0px") {
+
+      $(".navbar-content").animate({
+        "width" : "350px"
+      }, 300, function () {
+        $(".navbar-content > div").delay(500).fadeIn(300);
+      });
+
+    };
+    
+  });
+
+  $(".navbar-content-close-btn").click(function (event) {
+
+    $(".navbar-content > div").fadeOut(300, function () {
+      $(".navbar-content").animate({
+        width : "0"
+      }, 300);
+    });
+
+  });
+
+  $(".panel-heading").click(function (event) {
+    $(".panel-heading").removeClass("open");
+    $(this).addClass("open");
+  });
+});
+
 
 
 
