f5e98d685ec4dea646a59d579403200b7d96ef56
Bronson Dunbar <bronson@bronsondunbar.com>
2017-07-02 14:49:26 +0200

    Updates

--- source/js/app.js
+++ source/js/app.js
@@ -240,28 +240,6 @@ $(".panel-heading").click(function (event) {
   $(this).addClass("open");
 });
 
-$(window).scroll(function(){
-    // This is then function used to detect if the element is scrolled into view
-    function elementScrolled(elem)
-    {
-        var docViewTop = $(window).scrollTop();
-        var docViewBottom = docViewTop + $(window).height();
-        var elemTop = $(elem).offset().top;
-        return ((elemTop <= docViewBottom) && (elemTop >= docViewTop));
-    }
-     
-    // This is where we use the function to detect if ".box2" is scrolled into view, and when it is add the class ".animated" to the <p> child element
-    if(elementScrolled('.hideme')) {
-        var els = $('.hideme'),
-            i = 0,
-            f = function () {
-                $(els[i++]).velocity('transition.slideUpIn', { stagger: 700 }).delay(1000);
-                if(i < els.length) setTimeout(f, 400);
-            };
-        f();
-    }
-});
-
 
 
 
