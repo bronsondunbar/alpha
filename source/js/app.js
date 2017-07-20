1d149480eb5f90a92b34fa07390c83e8666ec0b6
bronsondunbar <bronson@proversity.org>
2017-07-20 17:06:10 +0200

    Updates

--- source/js/app.js
+++ source/js/app.js
@@ -14,28 +14,30 @@ $(document).ready(function () {
   $("body").waypoint(function() {
 
       $(".navbar-default").velocity({
-          backgroundColor : "#0D3758",
+          backgroundColor : "#ffffff",
           backgroundColorAlpha : 0
       }, 100);
 
+      $(".navbar-default").css("box-shadow", "none");
+
       $(".navbar-default .navbar-nav li a").velocity({
-        color: "#06A9E1"
+        color: "#0D3758"
       }, 10);
 
       $(".navbar-default .navbar-toggle .icon-bar").velocity({
-        backgroundColor: "#06A9E1"
+        backgroundColor: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > .fa-search").velocity({
-        color: "#06A9E1"
+        color: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > div > input").velocity({
-        borderBottomColor: "#06A9E1"
+        borderBottomColor: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > a").velocity({
-        color: "#06A9E1"
+        color: "#0D3758"
       }, 10);
 
   }, {
@@ -46,28 +48,30 @@ $(document).ready(function () {
   $(".waypoint").waypoint(function() {
  
       $(".navbar-default").velocity({
-          backgroundColor : "#0D3758",
+          backgroundColor : "#ffffff",
           backgroundColorAlpha : 1
       }, 100);
 
+      $(".navbar-default").css("box-shadow", "0px 4px 41px -5px rgba(0,0,0,0.49)");
+
       $(".navbar-default .navbar-nav li a").velocity({
-        color: "#ffffff"
+        color: "#0D3758"
       }, 10);
 
       $(".navbar-default .navbar-toggle .icon-bar").velocity({
-        backgroundColor: "#ffffff"
+        backgroundColor: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > .fa-search").velocity({
-        color: "#ffffff"
+        color: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > div > input").velocity({
-        borderBottomColor: "#ffffff"
+        borderBottomColor: "#0D3758"
       }, 10);
 
       $(".navbar-header > .search > a").velocity({
-        color: "#ffffff"
+        color: "#0D3758"
       }, 10);
 
   }, {
